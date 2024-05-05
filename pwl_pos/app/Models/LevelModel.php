f7d0e3d8e795d444e52e18567b3cca3478bb5212
GalaAldebara <lalasipo20@gmail.com>
2024-05-05 22:41:38 +0700

    Jobsheet 10 - Tugas

--- pwl_pos/app/Models/LevelModel.php
+++ pwl_pos/app/Models/LevelModel.php
@@ -2,18 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class LevelModel extends Model
 {
+    use HasFactory;
+
     protected $table = 'm_level';
     protected $primaryKey = 'level_id';
 
     protected $fillable = ['level_kode', 'level_nama'];
-    public function user(): BelongsTo
+
+    public function users(): BelongsTo
     {
-        return $this->belongsTo(UserModel::class);
+        return $this->belongsTo(UserModel::class, 'user_id', 'user_id');
     }
 }
